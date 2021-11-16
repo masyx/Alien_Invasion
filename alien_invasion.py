fab1a865f033a3a277716fea24470d189183792e
Sergey Matveev <rfmatveev@gmail.com>
2021-11-15 22:07:54 -0600

    refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,4 @@
+from _typeshed import Self
 import sys
 
 import pygame
@@ -20,18 +21,25 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()            
+
+            
+
+
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-
+                
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        
 
 if __name__ == '__main__':
     # Make a game instance and run the game
